556ae8bf90bb9e8510b0e6755d66ec2f15740cd9
MariaShebeko <marygro89@gmail.com>
2023-09-07 09:57:11 +0200

    Added Sqlite to create db with customers, and made basic CRUD operations

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -4,16 +4,18 @@ import WelcomeScreen from "../screens/WelcomeScreen";
 import SubscribeScreen from "../screens/SubscribeScreen";
 import MenuScreen from "../screens/MenuScreen";
 import UserSettingsScreen from "../screens/UserSettingsScreen";
+import CustomersScreen from "../screens/CustomersScreen";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="UserSettings">
+    <Stack.Navigator initialRouteName="Customers">
       <Stack.Screen name="Menu" component={MenuScreen} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Subscribe" component={SubscribeScreen} />
       <Stack.Screen name="UserSettings" component={UserSettingsScreen} />
+      <Stack.Screen name="Customers" component={CustomersScreen} />
     </Stack.Navigator>
   );
 };
